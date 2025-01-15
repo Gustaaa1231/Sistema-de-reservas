35749d067edb0d65e079956ad498440c63a92a4e
LuanEnrico30 <luanzinhoenrico@gmail.com>
2025-01-15 16:58:46 -0400

    feat: Input component/ input style.

--- components/InputComponent.php
+++ components/InputComponent.php
@@ -1,1 +1,33 @@
-<a href=""></a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="foo.php" method="post">
+        <label for="nome">
+            Nome:
+            <br>
+            <input type="text" name="username" /><br />
+        </label>
+        <label for="">
+            Email:
+            <br>
+            <input type="text" name="email" /><br />
+        </label>
+        <label for="">
+            Telefone:
+            <br>
+            <input type="tel" name="telefone" /><br />
+        </label>
+    </form>
+</body>
+</html>
+
+<style>
+
+
+
+</style>
