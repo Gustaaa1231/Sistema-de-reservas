82b225884f7e017b094d1dbbee3541866dd8353d
Gustaaa1231 <guguimaraes13@gmail.com>
2025-01-25 09:48:57 -0400

    fix: BD

--- backend/db/SistemaDeReservas.sql
+++ backend/db/SistemaDeReservas.sql
@@ -10,6 +10,22 @@ nome varchar(30),
 senha varchar(20)
 );
 
-insert into usuario(nome,senha) values 
-("Gusta", "123");
+create table locais(
+id int auto_increment primary key,
+nome varchar(20)
+);
+
+create table clientes(
+id int auto_increment primary key,
+nome varchar(30),
+telefone int unique,
+email varchar(40) unique
+);
+
+create table reservas(
+id int auto_increment primary key,
+id_cliente int,
+id_local int,
+data_reserva date
+);
 
